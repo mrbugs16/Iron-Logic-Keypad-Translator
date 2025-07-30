ad6d7bb431a9c4792d352127e5235d952ec31cea
Becario <becario@KAPLCS68-8.local>
2025-07-30 16:53:12 -0600

    feat: Translation logic

---
+++ src/IronLogicKeypad/OldPhonePad.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace IronLogicKeyPad
+{
+
+    public static class OldphonePadTranslator
+    {
+        private static readonly Dictionary<char, string> keyMap = new Dictionary<char, string>
+    {
+        { '1', "&'(" },
+        { '2', "ABC" },
+        { '3', "DEF" },
+        { '4', "GHI" },
+        { '5', "JKL" },
+        { '6', "MNO" },
+        { '7', "PQRS" },
+        { '8', "TUV" },
+        { '9', "WXYZ" },
+        { '0', " " },
+    };
+
+        public static String OldPhonePad(string input)
+        {
+
+            StringBuilder resultBuilder = new StringBuilder();
+            for (int i = 0; i < input.Length; i++)
+            {
+                char currentChar = input[i];
+
+                // SEND --> #
+                if (currentChar == '#')
+                {
+                    break;
+                }
+
+                // DELETE --> * 
+                if (currentChar == '*')
+                {
+                    if (resultBuilder.Length > 0)
+                    {
+                        resultBuilder.Length--;
+                    }
+                    continue;
+                }
+
+                // SPACE --> 0 
+                if (currentChar == '0')
+                {
+                    continue;
+                }
+
+                // PULSES 
+                int pressCount = 1;
+                while (i + 1 < input.Length && input[i + 1] == currentChar)
+                {
+                    pressCount++;
+                    i++;
+                }
+                if (keyMap.ContainsKey(currentChar))
+                {
+                    string charactersOnKey = keyMap[currentChar];
+                    int charIndex = (pressCount - 1) % charactersOnKey.Length;
+
+                    resultBuilder.Append(charactersOnKey[charIndex]);
+                }
+            }
+            return resultBuilder.ToString();
+        }
+    }
+}
--- src/IronLogicKeypad/Program.cs
+++ src/IronLogicKeypad/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using IronLogicKeyPad;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Your name: ");
+        string userName = Console.ReadLine();
+        Console.WriteLine("Write your sequence: ");
+        string sequence = Console.ReadLine();
+
+        string translatedText = OldphonePadTranslator.OldPhonePad(sequence);
+
+        Console.WriteLine("Tranlated Text : ");
+        Console.WriteLine(translatedText);
+    }
+}
